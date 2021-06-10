29634bdc1a5ac28b401ba53e844289f6ca70fd6e
Hideo Hattori <hhatto.jp@gmail.com>
2021-06-10 18:12:47 +0900

    add unittest for createCapSetterCaller

--- lib/policy/capSetter.spec.ts
+++ lib/policy/capSetter.spec.ts
@@ -1,4 +1,8 @@
+import Web3 from 'web3'
 import { createCapSetterCaller } from './capSetter'
+import { addresses } from '../addresses'
+import { contractFactory } from '../contract'
+import { policyAbi } from './abi'
 
 describe('capSetter.spec.ts', () => {
 	describe('createCapSetterCaller', () => {
@@ -44,6 +48,22 @@ describe('capSetter.spec.ts', () => {
 			const result = await caller().catch((err) => err)
 
 			expect(result).toEqual(error)
+		})
+
+		const itif = (condition: boolean): any => (condition ? it : it.skip)
+		const { WEB3_PROVIDER } = process.env
+		itif(WEB3_PROVIDER !== undefined)('call success with', async () => {
+			const web3 = new Web3(WEB3_PROVIDER || '')
+			const client = contractFactory(web3.currentProvider)
+			const address = await client
+				.registry(addresses.eth['main']?.registry)
+				['policy']()
+			const policyContract = new web3.eth.Contract([...policyAbi], address, {})
+
+			const caller = createCapSetterCaller(policyContract as any)
+			const result = await caller()
+
+			expect(result.startsWith('0x')).toEqual(true)
 		})
 	})
 })
